c097aefd763fb04b94af7f91b95d97a4795d42a4
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2023-11-21 00:13:57 -0800

    update script

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,13 @@
 FROM registry.redhat.io/rhel9/postgresql-15
-
 USER root
-RUN mkdir /opt/app-root && chmod 755 /opt/app-root
-WORKDIR /opt/app-root
+RUN mkdir /opt/app-root2 && chmod 755 /opt/app-root2
+WORKDIR /opt/app-root2
 ENV TZ=PST8PDT
 COPY . .
 ARG SOURCE_REPO=webdevops
 ARG GOCROND_VERSION=23.2.0
 ADD https://github.com/$SOURCE_REPO/go-crond/releases/download/$GOCROND_VERSION/go-crond.linux.amd64 /usr/bin/go-crond
-#USER root
 RUN chmod +x /usr/bin/go-crond
+RUN chmod +x /opt/app-root2/run.sh
 RUN echo $TZ > /etc/timezone
-USER 26
 ENTRYPOINT ["go-crond", "crontab", "--allow-unprivileged", "--verbose", "--log.json"]
--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-data_dir="/data"
-cd $data_dir
-
-pg_dump -U $PGUSER -h localhost -p 6003 $PGDATABASE -n public --format=p  --file=legal-backup.sql
-psql -U $REPLICA_ADMIN -h localhost -p 5432 -ac "DROP DATABASE \"${PGDATABASE}\";"
-psql -U $REPLICA_ADMIN -h localhost -p 5432 -ac "CREATE DATABASE \"${PGDATABASE}\";"
-psql -U $PGUSER -h localhost -p 5432 -ac "GRANT ALL ON DATABASE \"${PGDATABASE}\" TO \"${PGUSER}\";"
-psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -v ON_ERROR_STOP=0 -x < legal-backup.sql
-#rm legal-backup.sql
+pg_dump -U $PGUSER -h localhost -p 6003 $PGDATABASE -n public --format=p  --file=/data/legal-backup.sql
+dropdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
+createdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
+# psql -U $PGUSER -h localhost -p 5432 -ac "GRANT ALL ON DATABASE \"${PGDATABASE}\" TO \"${PGUSER}\";"
+psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -v ON_ERROR_STOP=0 -x < /data/legal-backup.sql
+#rm /data/legal-backup.sql
