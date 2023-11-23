071632c13ce0a142f5dfd6a1f56e5be527e6e8a0
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2023-11-22 20:54:29 -0800

    generalize

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-pg_dump -U $PGUSER -h localhost -p 6003 $PGDATABASE -n public --format=p  --file=/data/legal-backup.sql
+pg_dump -U $PGUSER -h localhost -p 6003 $PGDATABASE -n public --format=p  --file=/data/backup.sql
 dropdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
 createdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
-# psql -U $PGUSER -h localhost -p 5432 -ac "GRANT ALL ON DATABASE \"${PGDATABASE}\" TO \"${PGUSER}\";"
-psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -v ON_ERROR_STOP=0 -x < /data/legal-backup.sql
+psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -v ON_ERROR_STOP=0 -x < /data/backup.sql
 psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -ac "CREATE USER readonly WITH PASSWORD '${READONLY_PASSWORD}';"
 psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -ac "GRANT CONNECT ON DATABASE \"${PGDATABASE}\" to readonly;"
 psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -ac "GRANT USAGE ON SCHEMA public TO readonly;"
