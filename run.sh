e69574fa4aa2d7a5607c14a0df4622762d200adf
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2025-05-07 15:15:39 -0700

    error

--- run.sh
+++ run.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 pg_dump -U $PGUSER -h localhost -p 6003 $PGDATABASE -n public --format=p  --file=/data/backup.sql
 psql -U $PGUSER -h localhost -p 6003 -d postgres --tuples-only --no-align -c "
-  SELECT 'CREATE ROLE IF NOT EXISTS \"' || rolname || '\"' ||
-         CASE WHEN rolcanlogin AND rolname IN ('readonly', 'app_user') 
-              THEN ' WITH LOGIN' 
-              ELSE ' WITH NOLOGIN' 
-         END || ';'
-  FROM pg_roles 
+  SELECT 'DO \$\$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = ''' || rolname || ''') THEN
+    CREATE ROLE \"' || rolname || '\"' ||
+    CASE WHEN rolcanlogin AND rolname IN ('readonly', 'app_user')
+         THEN ' WITH LOGIN'
+         ELSE ' WITH NOLOGIN'
+    END || ';
+  END IF;
+END
+\$\$;'
+  FROM pg_roles
   WHERE rolname NOT IN ('postgres', 'cloudsqladmin', 'cloudsqlsuperuser')
     AND rolname NOT LIKE 'pg\\_%' ESCAPE '\\'
     AND rolname NOT LIKE 'iam\\_%' ESCAPE '\\'
     AND rolname NOT LIKE 'rds%'
     AND rolname NOT LIKE 'cloudsql%'
-" | grep -v "CREATE ROLE IF NOT EXISTS \"\"" > /data/roles.sql
+" | grep -v "^DO \$\$" > /data/roles.sql
 psql -U $REPLICA_ADMIN -h localhost -p 5432 -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$PGDATABASE' AND pid <> pg_backend_pid();"
 dropdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
 createdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
