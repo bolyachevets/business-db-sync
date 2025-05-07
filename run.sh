dac811f8097bc4e8b124800dc475f1b277645cc8
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2025-05-07 14:55:25 -0700

    roles

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 pg_dump -U $PGUSER -h localhost -p 6003 $PGDATABASE -n public --format=p  --file=/data/backup.sql
-pg_dumpall -U $PGUSER -h localhost -p 6003 --roles-only > /data/roles.sql
+psql -U $PGUSER -h localhost -p 6003 -d postgres -c "
+  SELECT 'CREATE ROLE \"' || rolname || '\"' || 
+         CASE WHEN rolcanlogin AND rolname IN ('readonly', 'app_user') 
+              THEN ' WITH LOGIN' 
+              ELSE ' WITH NOLOGIN' 
+         END || ';'
+  FROM pg_roles 
+  WHERE rolname NOT IN ('postgres', 'cloudsqladmin', 'cloudsqlsuperuser')
+    AND rolname NOT LIKE 'pg\\_%' ESCAPE '\\'
+    AND rolname NOT LIKE 'iam\\_%' ESCAPE '\\'
+    AND rolname NOT LIKE 'rds%'
+    AND rolname NOT LIKE 'cloudsql%'
+" | grep -v "CREATE ROLE \"\"" > /data/roles.sql
 psql -U $REPLICA_ADMIN -h localhost -p 5432 -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$PGDATABASE' AND pid <> pg_backend_pid();"
 dropdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
 createdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
