b1aa8b257ac8db6d40c67d35cb22e0003b9eb838
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2025-05-07 15:07:43 -0700

    fix errors

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pg_dump -U $PGUSER -h localhost -p 6003 $PGDATABASE -n public --format=p  --file=/data/backup.sql
-psql -U $PGUSER -h localhost -p 6003 -d postgres -c "
-  SELECT 'CREATE ROLE \"' || rolname || '\"' || 
+psql -U $PGUSER -h localhost -p 6003 -d postgres --tuples-only --no-align -c "
+  SELECT 'CREATE ROLE IF NOT EXISTS \"' || rolname || '\"' ||
          CASE WHEN rolcanlogin AND rolname IN ('readonly', 'app_user') 
               THEN ' WITH LOGIN' 
               ELSE ' WITH NOLOGIN' 
@@ -12,7 +12,7 @@ psql -U $PGUSER -h localhost -p 6003 -d postgres -c "
     AND rolname NOT LIKE 'iam\\_%' ESCAPE '\\'
     AND rolname NOT LIKE 'rds%'
     AND rolname NOT LIKE 'cloudsql%'
-" | grep -v "CREATE ROLE \"\"" > /data/roles.sql
+" | grep -v "CREATE ROLE IF NOT EXISTS \"\"" > /data/roles.sql
 psql -U $REPLICA_ADMIN -h localhost -p 5432 -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$PGDATABASE' AND pid <> pg_backend_pid();"
 dropdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
 createdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
