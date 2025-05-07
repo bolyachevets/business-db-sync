7c061c198dd433dba287ccab2893c382d76ed2b1
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2025-05-07 13:29:21 -0700

    test role import

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 pg_dump -U $PGUSER -h localhost -p 6003 $PGDATABASE -n public --format=p  --file=/data/backup.sql
+pg_dumpall -U $PGUSER -h localhost -p 6003 --roles-only > /data/roles.sql
+psql -U $REPLICA_ADMIN -h localhost -p 5432 -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '$PGDATABASE' AND pid <> pg_backend_pid();"
 dropdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
 createdb -U $REPLICA_ADMIN -h localhost -p 5432 $PGDATABASE
+psql -U $REPLICA_ADMIN -h localhost -p 5432 -f /data/roles.sql
 psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -v ON_ERROR_STOP=0 -x < /data/backup.sql
-psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -ac "CREATE USER readonly WITH PASSWORD '${READONLY_PASSWORD}';"
-psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -ac "GRANT CONNECT ON DATABASE \"${PGDATABASE}\" to readonly;"
-psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -ac "GRANT USAGE ON SCHEMA public TO readonly;"
-psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -ac "GRANT SELECT ON ALL TABLES IN SCHEMA public to readonly;"
-psql -U $REPLICA_ADMIN -h localhost -p 5432  -d $PGDATABASE -ac "ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly;"
+psql -U $REPLICA_ADMIN -h localhost -p 5432 -c "ALTER USER readonly WITH LOGIN PASSWORD '${READONLY_PASSWORD}';"
