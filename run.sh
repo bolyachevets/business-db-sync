a0ec371587b5549372ea92181d40a6222fc96694
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2026-06-17 15:49:56 -0700

    attempt to use stream

--- run.sh
+++ run.sh
@@ -22,11 +22,6 @@ psql -U $PGUSER -h localhost -p 6003 -d $PGDATABASE --tuples-only --no-align -c 
   WHERE n.nspname = '$SCHEMA'
 " > /data/extensions.sql
 
-# Remove backslash metacommands (but keep \. COPY terminators) and transaction blocks
-sed -i '/^\\[^.]/ d' /data/backup.sql
-sed -i '/^BEGIN;/d' /data/backup.sql
-sed -i '/^COMMIT;/d' /data/backup.sql
-
 # Kill connections
 while
   psql -U $REPLICA_ADMIN -h localhost -p 5432 -d postgres -tAc "SELECT 1 FROM pg_stat_activity WHERE datname = '$PGDATABASE' AND pid <> pg_backend_pid()" | grep -q 1
@@ -45,10 +40,10 @@ psql -U $REPLICA_ADMIN -h localhost -p 5432 -d $PGDATABASE -c "CREATE SCHEMA IF 
 # Create extensions (if any)
 [ -s /data/extensions.sql ] && psql -U $REPLICA_ADMIN -h localhost -p 5432 -d $PGDATABASE -f /data/extensions.sql
 
-# Restore with session_replication_role
+# Restore with session_replication_role (filter during restore to save space)
 {
   echo "SET session_replication_role = 'replica';"
-  cat /data/backup.sql
+  sed '/^\\[^.]/ d; /^BEGIN;/d; /^COMMIT;/d' /data/backup.sql
   echo "SET session_replication_role = 'origin';"
 } | psql -U $REPLICA_ADMIN -h localhost -p 5432 -d $PGDATABASE -v ON_ERROR_STOP=0
 
